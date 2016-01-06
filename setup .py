8d3cb86fd8b834f27b9bb9f0f8b13e55537a7347
monptitnuage <monptitnuage.webmaster@gmail.com>
2016-01-06 12:26:21 +0100

    Create setup .py

---
+++ setup .py
@@ -0,0 +1,6 @@
+from cx_Freeze import setup, Executable
+setup(
+        name = "Pings",
+        version = "1.0",
+        description = "utilitaire_reseau",
+        executables = [Executable("C:\\Python27\\Scripts\\Pings.py")])
