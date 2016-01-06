6b37671bbf35fef184d31db200582c2a83c787dd
monptitnuage <monptitnuage.webmaster@gmail.com>
2016-01-06 12:41:21 +0100

    Update setup .py

--- setup .py
+++ setup .py
@@ -3,4 +3,4 @@ setup(
         name = "Pings",
         version = "1.0",
         description = "utilitaire_reseau",
-        executables = [Executable("C:\\Python27\\Scripts\\Pings.py")])
+        executables = [Executable("Pings.py")])
